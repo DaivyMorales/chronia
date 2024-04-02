f9b2fc941cd8e68f378371ce62e71f40fc98ddfa
DaivyMorales <daivymorales7705@gmail.com>
2024-04-02 15:06:37 -0500

    feat(auth): login with Google account

--- src/pages/auth/signin/index.tsx
+++ src/pages/auth/signin/index.tsx
@@ -1,15 +1,21 @@
 import { FaXTwitter } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
+import { signIn } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 function SignIn() {
+  const { data: session } = useSession();
+
+  console.log(session);
+
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
       <h3>Sign in to Chronia</h3>
-      <button className="signin-button">
-        <FaXTwitter /> Sign in with Twitter
-      </button>
-      <button className="signin-button">
-        <FcGoogle /> Sign in with Google
+      {/* <button onClick={() => signIn("twitter")} className="signin-button">
+        <FaXTwitter /> Continue with Twitter
+      </button> */}
+      <button onClick={() => signIn("google")} className="signin-button">
+        <FcGoogle /> Continue with Google
       </button>
     </div>
   );
