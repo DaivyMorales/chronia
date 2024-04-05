9b8bf38e3af6de61bbf6602fe5d33688a87c7a06
DaivyMorales <daivymorales7705@gmail.com>
2024-04-05 17:51:15 -0500

    feat(example): example page UI

---
+++ src/pages/components/InputQuestion.tsx
@@ -0,0 +1,58 @@
+import React, { ChangeEvent, useState, useRef, useEffect } from "react";
+import { motion } from "framer-motion";
+
+interface InputQuestionProps {
+  index: number;
+}
+
+function InputQuestion({ index }: InputQuestionProps) {
+  const [value, setValue] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(e.target.value);
+    autoAdjustHeight(e);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  };
+
+  const autoAdjustHeight = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    const textarea = event.target;
+    textarea.style.height = "auto";
+    textarea.style.height = `${textarea.scrollHeight}px`;
+  };
+
+  return (
+    <motion.div
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      // transition={{duration: 0.3}}
+      className="flex items-center gap-2"
+    >
+      <p
+        className={`${isFocused ? "text-neutral-100" : ""} font-semibold text-neutral-500`}
+      >
+        {index + 1}.
+      </p>
+      <textarea
+        ref={textareaRef}
+        value={value}
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+        rows={1}
+        onMouseEnter={() => setIsFocused(true)}
+        onMouseLeave={() => setIsFocused(false)}
+        className={` ${isFocused ? "" : "text-neutral-500"} textarea-question w-[363px] resize-none whitespace-normal rounded-lg  bg-transparent px-4 py-2 text-sm font-normal text-neutral-200 placeholder:text-neutral-500`}
+        placeholder="Am I more confident?"
+      />
+    </motion.div>
+  );
+}
+
+export default InputQuestion;
--- src/pages/example/index.tsx
+++ src/pages/example/index.tsx
@@ -1,35 +1,136 @@
 import React, { useState } from "react";
 import { HiPlus } from "react-icons/hi";
+import InputQuestion from "../components/InputQuestion";
+import { FaArrowRight } from "react-icons/fa6";
+import { motion } from "framer-motion";
+import {
+  TiHeartOutline,
+  TiGroupOutline,
+  TiChartBarOutline,
+} from "react-icons/ti";
+
+interface QuestionProps {
+  id: number;
+  question: string;
+}
 
 function Example() {
-  const [quiestions, setQuiestions] = useState([]);
+  const [question, setQuestion] = useState<QuestionProps[]>([
+    {
+      id: 1,
+      question: "",
+    },
+  ]);
 
-  const DecirHola = () => {
-    console.log("hola");
-  };
-
-  
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-between gap-3 p-3">
-      <div>Chronia</div>
+    <div className="relative flex h-screen w-full flex-col items-center justify-between gap-3 p-3">
+      {/* <div className="absolute bg-rose-600 w-screen h-[100px] blur-[90px]"/> */}
+      {/* <h2 className="z-50">Chronia</h2> */}
+      <h2>Your goals</h2>
       {/* <textarea name="" id="" cols={60} rows={30}></textarea>
       <button>Yes!</button> */}
-      <div className="flex flex-col items-start justify-start gap-4 rounded-xl  border-neutral-200 p-4 ">
-        <div className="rounded-full border-[1px] border-rose-200 bg-rose-100 px-3 py-[1px]">
-          <p className="text-sm font-bold text-rose-400">Love</p>
+
+      <div className="grid grid-cols-1 gap-5 lg:grid-cols-1">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          className="b] z-50  flex flex-col items-start justify-start gap-4 rounded-xl border-neutral-200 border-neutral-900  p-9 "
+        >
+          <div className="rounded-xl bg-gradient-to-r from-pink-600 to-rose-700 p-1">
+            <TiHeartOutline size={30} />
+          </div>
+          <h3 className="flex items-center gap-1 font-bold text-white">
+            Love / Relationships
+          </h3>
+
+          <div className="flex w-full flex-col items-center justify-center  gap-2">
+            {question.map((q, index) => (
+              <InputQuestion index={index} />
+            ))}
+            {question.length < 5 && (
+              <div className="w-full ">
+                <motion.button
+                whileHover={{scale: 1.1}}
+                onClick={() => {
+                  setQuestion([
+                    ...question,
+                    {
+                      id: 0,
+                      question: "",
+                    },
+                  ]);
+                }}
+                className="rounded-full border-[1px] border-neutral-500 p-1 text-[8px] text-neutral-400 hover:border-neutral-200 hover:text-neutral-200"
+              >
+                <HiPlus/>
+              </motion.button>
+              </div>
+            )}
+          </div>
+        </motion.div>
+
+        {/* <div className="z-50 flex  flex-col items-start justify-start gap-4 rounded-xl border-[1px] border-neutral-200 border-neutral-900 bg-[#170F23] p-9 ">
+          <div className="rounded-xl bg-gradient-to-r from-sky-600 to-blue-700 p-1">
+            <TiGroupOutline size={30} />
+          </div>
+          <h3 className="flex items-center gap-1  font-bold text-white">
+            Social
+          </h3>
+        
+          <div className="flex w-full flex-col items-center justify-center  gap-2">
+            {question.map((q, index) => (
+              <InputQuestion index={index} />
+            ))}
+            {question.length < 5 && (
+              <button
+                onClick={() => {
+                  setQuestion([
+                    ...question,
+                    {
+                      id: 0,
+                      question: "",
+                    },
+                  ]);
+                }}
+                className="rounded-full border-[2px] p-1"
+              >
+                <HiPlus size={13} color="gray" />
+              </button>
+            )}
+          </div>
         </div>
-        <div className="">
-          <h3>What you would like to ask yourself in 3 months?</h3>
-          <p className="text-neutral-400">
-            Write max 5 question that you want to do yourself
-          </p>
-        </div>
-        <div className="flex w-full items-center justify-start  gap-2">
-          <input type="text" className="question-input" />{" "}
-          <button className="rounded-full border-[1px] p-1">
-            <HiPlus size={13} color="gray" />
-          </button>
-        </div>
+
+        <div className="z-50 flex  flex-col items-start justify-start gap-4 rounded-xl border-[1px] border-neutral-200 border-neutral-900 bg-[#170F23] p-9 ">
+          <div className="rounded-xl bg-green-600 p-1">
+            <TiChartBarOutline size={30} />
+          </div>
+          <h3 className="flex items-center gap-1  font-bold text-white">
+            Finanze
+          </h3>
+        
+          <div className="flex w-full flex-col items-center justify-center  gap-2">
+            {question.map((q, index) => (
+              <InputQuestion index={index} />
+            ))}
+            {question.length < 5 && (
+              <button
+                onClick={() => {
+                  setQuestion([
+                    ...question,
+                    {
+                      id: 0,
+                      question: "",
+                    },
+                  ]);
+                }}
+                className="rounded-full border-[2px] p-1"
+              >
+                <HiPlus size={13} color="gray" />
+              </button>
+            )}
+          </div>
+        </div> */}
       </div>
 
       {/* <div className="flex flex-col items-start justify-start gap-2 rounded-xl border-[1px] border-neutral-200 p-4 shadow-sm">
@@ -39,7 +140,14 @@ function Example() {
         <h4>What is your goal in 6 months?</h4>
         <input type="text" className="text-xs" />
       </div> */}
-      <button className="next-button">Continue</button>
+      <motion.button
+        initial={{ scale: 1 }}
+        whileHover={{ scale: 1.25 }}
+        whileTap={{ scale: 0.7 }}
+        className="next-button"
+      >
+        Continue <FaArrowRight />
+      </motion.button>
     </div>
   );
 }
