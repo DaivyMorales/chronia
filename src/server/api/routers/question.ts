585973b8644b7c72fd7b88582b0759f5795e7517
DaivyMorales <daivymorales7705@gmail.com>
2024-04-08 15:13:33 -0500

    feat: createQuestion logic

---
+++ src/server/api/routers/question.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+} from "@/server/api/trpc";
+
+export const questionArea = createTRPCRouter({
+  createQuestion: protectedProcedure
+    .input(
+      z.object({
+        areaId: z.string(),
+        question_description: z.string(),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.db.question.create({
+        data: {
+          question_description: input.question_description,
+          areaId: input.areaId,
+        },
+      });
+    }),
+});
